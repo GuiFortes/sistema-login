3b9bc38d1ba060033245be8cae0eb2fc1901a9fe
GUILHERME FORTES STEFANI <03835438000@senacrs.edu.br>
2025-10-03 09:37:39 -0300

    primeiro commit

---
+++ src/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root', //seu usuário
+  password: 'root', //sua senha
+  database: 'escola' //nome do banco
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Erro de conexão: ', err);
+        return;
+    }
+    console.log('Conectado ao MySQL!');
+})
+module.exports = db;
